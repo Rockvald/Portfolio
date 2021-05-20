3e5e45fe25adf716d94980aedb43badebfe910fd
Rockvald <mathieuprot@orange.fr>
2021-05-20 17:21:24 +0200

    Ajout de la base des pages

---
+++ js/fonctions.js
@@ -0,0 +1,12 @@
+function menu() {
+    var menu = document.getElementById("menu");
+    var corpsScrollTop = document.documentElement.scrollTop;
+    var headerOffsetHeight = document.getElementById("header").offsetHeight;
+    if (corpsScrollTop > headerOffsetHeight) {
+        menu.classList.remove("menu-normal");
+        menu.classList.add("menu-top");
+    } else {
+        menu.classList.add("menu-normal");
+        menu.classList.remove("menu-top");
+    }
+}
